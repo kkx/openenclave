dd8a7576d75d6a6317111f948e9d5df46cf00630
John Kordich <johnkord@microsoft.com>
2019-03-07 14:47:36 -0800

    WIP 2

--- tests/qv/host/host.c
+++ tests/qv/host/host.c
@@ -5,7 +5,6 @@
 #include <openenclave/host.h>
 #include <openenclave/internal/error.h>
 #include <openenclave/internal/tests.h>
-#include <common/sgx/quote.h>
 #include <openenclave/bits/report.h>
 #include <openenclave/internal/report.h>
 #include <stdio.h>
@@ -27,7 +26,7 @@ int main(int argc, const char* argv[])
     size_t fsize;
 
     // Positive test
-    ptr = fopen("/home/oeadmin/generated_report.bytes","rb");
+    ptr = fopen("/home/johnkord/generated_report.bytes","rb");
     OE_TEST(ptr != NULL);
 
     fseek(ptr, 0, SEEK_END);
@@ -42,6 +41,20 @@ int main(int argc, const char* argv[])
     OE_TEST(retval1 == OE_OK);
 
 
+    // Negative test
+    ptr = fopen("/home/johnkord/generated_report.bytes_bad","rb");
+    OE_TEST(ptr != NULL);
+
+    fseek(ptr, 0, SEEK_END);
+    fsize = (size_t)ftell(ptr);
+    fseek(ptr, 0, SEEK_SET);
+
+    OE_TEST(fread(buffer, fsize, 1, ptr) > 0);
+    fclose(ptr);
+
+    retval1 = oe_verify_report(NULL, buffer, fsize, &oe_report);
+
+    OE_TEST(retval1 != OE_OK);
     
 
     printf("=== passed all tests (qv)\n");
